4d45ce8816e5565da89eb95d3d4b89fda7f3fb93
Tatyana Shuniborova <tashu529@gmail.com>
2021-03-28 16:40:10 +0300

    feat: add solution vigenere code

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,60 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    
+  constructor(direct = true) {
+      this.direct = direct;
+  }
+
+  encrypt(str, key) {
+    
+      if (!str || !key) {
+          throw new Error();
+      }
+
+      let massStr = str.toUpperCase().split('');
+      let massKey = key.toUpperCase().split('');
+
+      let result = massStr.map(element => {
+          let itog = element;
+          let code = element.charCodeAt();
+          
+          if (code >= 65 && code <= 90) {
+            itog = String.fromCharCode(((code + massKey[0].charCodeAt()) % 26) + 65);
+              massKey.push(massKey.shift());
+          }
+          return itog;
+      });
+
+      if (!this.direct) {
+          result.reverse();     
+      } 
+
+      return result.join('');
+  }
+
+  decrypt(str, key) {
+
+      if (!str || !key) {
+          throw new Error();
+      }
+
+      let massStr = str.toUpperCase().split('');
+      let massKey = key.toUpperCase().split('');
+
+      let result = massStr.map(el => {
+          let itog = el;
+          let code = el.charCodeAt();
+          if (code >= 65 && code <= 90) {
+            itog = String.fromCharCode(((code - (massKey[0].charCodeAt()) + 26) % 26) + 65);
+              massKey.push(massKey.shift());
+          }
+          return itog;
+      });
+
+      if (!this.direct) {
+          result.reverse();     
+      } 
+
+      return result.join('');
   }
 }
 
